a093185aac78aaff9aada7f67745625747c37c95
Parv Saxena <72391078+parv-web@users.noreply.github.com>
2021-05-17 17:34:22 +0530

    Update issue.js

--- test/issue.js
+++ test/issue.js
@@ -6,7 +6,7 @@ const zapier = require('zapier-platform-core');
 const App = require('../index');
 const appTester = zapier.createAppTester(App);
 
-const REPO = 'username/reponame' // CHANGE THIS
+const REPO = 'parv-web/parv-zapier' // CHANGE THIS
 
 //These are automated tests for the Issue create and Issue Trigger.
 //They will run every time the `zapier test` command is executed.
